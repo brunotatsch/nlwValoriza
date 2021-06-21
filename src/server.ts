c0ca224e3797cebabb4d009c8fdba71f2b36470d
Bruno Tatsch <brunotatsch@gmail.com>
2021-06-21 20:37:36 -0300

    install typeorm / sqlite database with conection

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,8 @@
+import  "reflect-metadata";
 import express, { request, response } from "express";
+
+import "./database";
 
 const app = express();
 
-/**
- * GET    => Buscar / Pesquisa
- * POST   => Inserir / Criar uma informação
- * PUT    => Alterar/Update uma informação
- * DELETE => Apagar/Remover uma informação
- * PET    => Alterar/update uma informação especifica
- */
-
-app.get('/teste', (request, response) => {
-  
-  // request : inbound data
-  // respose : outbound data
-
-  return response.send("Olá NLW")
-
-});
-
-app.post("/teste-post", (request, response) => {
-
-  return response.send("Olá NLW by post Api")
-
-} )
-
-app.listen(3000,() => console.log("Server Running NLW VI... Port: 3000"))
+app.listen(3000, () => console.log("Server Running NLW VI... Port: 3000"))
